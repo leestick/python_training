ca63baeb3cc1467e6fb5c93fde970e123e04d68d
leestick <leestick@mail.ru>
2018-10-31 00:23:12 +0300

    conftest

---
+++ test/conftest.py
@@ -0,0 +1,12 @@
+__author__ = 'ioK'
+
+import pytest
+from fixture.application import Application
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
